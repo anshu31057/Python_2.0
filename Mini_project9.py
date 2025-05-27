4cadbd6dfebc9bc8e6d95505d35ba30c50377d45
jerry74114 <anshubeniwal669@gmail.com>
2025-05-28 00:09:31 +0530

    Create Mini_project9.py

---
+++ Mini_project9.py
@@ -0,0 +1,45 @@
+contacts = {}
+while True:
+    print("PHONE DIARY")
+    print('''1. Add
+2. Search
+3. Delete
+4. View all
+5. Exit''')
+    print("---------------------------")
+    print("Enter Your Choice Down")
+    print("---------------------------")
+    opt = int(input(":"))
+    if opt == 1:
+        print("\nEnter 1234 to go back to main menu")
+        while True:
+            phone = int(input("Enter Contact number: "))
+            if phone == 1234:
+                break
+            name = input("Enter Name: ")
+            if name == 1234:
+             break
+            contacts[name] = phone
+            print("Contact Saved Successfully!!")
+
+    elif opt ==2:
+        print("Search By Name or Phone no.")
+        print('''1. Name
+                 2. Phone no.''')
+        a1 = int(input(": "))
+        if a1 == 1:
+            b1 = input("Enter Name: ")
+            print(":",contacts.get(b1))
+        elif a1 == 2:
+            b2 = input("Enter Number: ")
+            for key , values in contacts.items():
+                if values == b2:
+                    print(":",key)
+    elif opt ==3:
+        a2 = input("Enter Name to delete:")
+        contacts.pop(a2,None)
+    elif opt == 4:
+        for phone , name in contacts.items():
+            print(phone,":",name)
+    elif opt ==5:
+        break
